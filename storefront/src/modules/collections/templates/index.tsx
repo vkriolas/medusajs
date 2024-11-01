0b47d9c76ff8d13df6c123776345ebffb6aec895
Rafael Soares <motionext.app@gmail.com>
2024-11-01 16:38:16 +0000

    General updates

--- storefront/src/modules/collections/templates/index.tsx
+++ storefront/src/modules/collections/templates/index.tsx
@@ -39,3 +39,5 @@ export default function CollectionTemplate({
     </div>
   )
 }
+
+export const dynamic = "force-dynamic"
--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,22 +1,27 @@
 import { Heading } from "@medusajs/ui"
+import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div 
-      className="h-[75vh] w-full border-b border-ui-border-base relative bg-cover bg-center"
-      style={{ backgroundImage: `url('https://i.ytimg.com/vi/ZuqatDVmbPo/maxresdefault.jpg')`, backgroundSize: "cover" }}
-    >
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative">
+      <Image
+        src="/hero-image.jpg"
+        alt="Hero background"
+        fill
+        className="object-cover"
+        priority
+      />
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-white font-normal"
+            className="text-3xl leading-10 text-white font-extrabold"
           >
             Revive the Style, Rewrite Fashion
           </Heading>
           <Heading
             level="h3"
-            className="text-3xl leading-10 text-slate-100 font-normal mt-2"
+            className="text-3xl leading-10 text-slate-100 font-semibold mt-2"
           >
             Discover unique, vintage pieces that celebrate authenticity and heritage. Let your style shine with the timeless elegance of Kriolas Vintage.
           </Heading>
