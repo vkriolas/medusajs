afeab0dfc8ec629996677d3cfa05c8de229ce34f
Rafael Soares <motionext.app@gmail.com>
2024-11-01 02:57:48 +0000

    Update hero

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,11 +1,10 @@
 import { Heading } from "@medusajs/ui"
-import Image from "../../public/main-hero.jpg"
 
 const Hero = () => {
   return (
     <div 
       className="h-[75vh] w-full border-b border-ui-border-base relative bg-cover bg-center"
-      style={{ backgroundImage: `url(${Image.src})`, backgroundSize: "cover" }}
+      style={{ backgroundImage: `url('https://i.ytimg.com/vi/ZuqatDVmbPo/maxresdefault.jpg')`, backgroundSize: "cover" }}
     >
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
